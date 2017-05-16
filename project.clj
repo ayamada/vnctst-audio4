a2c9d7a82349bf21ce0b7b6ccbd19e8e2d5a9e48
atsuo yamada <yamada-git2@tir.jp>
2017-05-16 13:33:21 +0900

    Released 0.2.1

--- project.clj
+++ project.clj
@@ -48,7 +48,7 @@
                [['figwheel figwheel-version]])))
 
 
-(defproject jp.ne.tir/vnctst-audio4 "0.2.1"
+(defproject jp.ne.tir/vnctst-audio4 "0.2.2-SNAPSHOT"
   :min-lein-version "2.5.0"
   :description "Audio playback library for html5 game"
   :url "https://github.com/ayamada/vnctst-audio4"
